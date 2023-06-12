0388f371132344d703c873ac693689eb94606556
menciatarinupv <127866315+menciatarinupv@users.noreply.github.com>
2023-06-12 19:12:52 +0200

    pasa de pantalla  al darle a enviar

--- UX/app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ UX/app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tematica;
+    private EditText textoPalabra;
+    private Intent enviar;
+    private String stringPalabra;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("onCreate", "estoy en onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tematica = (TextView) findViewById(R.id.tematica);
+        textoPalabra = (EditText) findViewById(R.id.palabra);
+
+
+    }
+
+    public void botonEnviar_pulsado (View quien){
+        Log.d("botón", "Botón pulsado");
+
+        //intent `para empezar la otra actividad
+        enviar = new Intent(this, pantalla6.class);
+
+        //empieza la actividad
+        this.startActivity(enviar);
+
+
     }
 }
---
+++ UX/app/src/main/java/com/example/proyectofinal/pantalla6.java
@@ -0,0 +1,25 @@
+package com.example.proyectofinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+public class pantalla6 extends AppCompatActivity{
+private String palabraRecibida;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla6);
+
+        //recibe el stringPalabra mandado de la actividad 1
+
+
+
+
+
+    }
+}
